fda43e2b4c1d2b31cc891e5c005e81e545b6be2b
Krzysztof Smigielski <coptercopter@users.noreply.github.com>
2017-02-20 21:43:11 +0100

    GitHub data update. Version 1.0-SNAPSHOT.

--- src/test/java/com/nilcaream/utilargs/BooleanIntegerFunctionalTest.java
+++ src/test/java/com/nilcaream/utilargs/BooleanIntegerFunctionalTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2013 Krzysztof Smigielski
+ * Copyright 2017 Krzysztof Smigielski
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
